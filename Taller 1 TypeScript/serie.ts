285c4fbd0a7a9207025c23da650f374b1af11859
Laura Avelino <l.avelino@uniandes.edu.co>
2025-10-17 13:25:32 -0500

    comienzo

---
+++ Taller 1 TypeScript/serie.ts
@@ -0,0 +1,18 @@
+
+
+export class Estudiante {
+  nombre: string;
+  canal: string;
+  temporadas: number;
+
+  constructor(nom:string,can:string,temp:number){
+    this.nombre=nom;
+    this.canal=can;
+    this.temporadas=temp;
+    
+
+  }
+  
+}
+
+
